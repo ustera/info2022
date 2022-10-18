17ec3018ff2fa68cd3d4b2b52d1271a3e8be1b3c
ustera <zalivina01@mail.ru>
2022-10-18 21:33:31 +0300

    Update import_data.py

--- final/import_data.py
+++ final/import_data.py
@@ -1,7 +1,6 @@
 import json
 from transformers import AutoTokenizer, AutoModel
 import pymorphy2
-from nltk.corpus import stopwords
 import pickle
 import streamlit as st
 
@@ -21,8 +20,7 @@ def import_files():
     with open('data/vectorizer_tfidf.pickle', 'rb') as pkl:
         vectorizer_tfidf = pickle.load(pkl)
     morph = pymorphy2.MorphAnalyzer()
-    stoplist = stopwords.words('russian')
-    return texts, vectorizer, bert_matrix_answers, matrix_bm25, morph, stoplist, matrix_tfidf, vectorizer_tfidf
+    return texts, vectorizer, bert_matrix_answers, matrix_bm25, morph, matrix_tfidf, vectorizer_tfidf
 
 
 def import_bert():
